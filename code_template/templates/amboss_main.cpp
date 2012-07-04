9fa0565037ab45d2ce59e96ad08eda5508b92653
Karsten Ahnert <karsten.ahnert@gmx.de>
2012-07-04 21:01:33 +0200

    added more templates

---
+++ code_template/templates/amboss_main.cpp
@@ -0,0 +1,20 @@
+/*
+ * $FILENAME
+ * Date: $DATE
+ * Author: $AUTHOR ($AUTHOREMAIL)
+ * Copyright: $AUTHOR
+ *
+ * Use, modification and distribution is subject to the Boost Software License,
+ * Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main( int argc , char **argv )
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
