eeb4e1fb7e00a01919aa076de25321a724a51bc2
Karsten Ahnert <karsten.ahnert@gmx.de>
2012-09-10 07:17:26 +0200

    added jamfile template

--- code_template/templates/simple_jamfile.py
+++ code_template/templates/simple_jamfile.py
@@ -7,25 +7,26 @@ from string import Template
 import code_template_helpers 
 
 
-
-
 filename_help = "Output file name(s)"
 target_help = "Cpp targets to be created."
 template = """# Date: $DATE
 # Author: $AUTHOR ($AUTHOREMAIL)
 
-CC = g++
-CXX = g++
+import boost ;
 
-INCLUDES = -I$$(BOOST_ROOT)
-CXXFLAGS += $$(INCLUDES)
+boost.use-project ;
 
-all : $TARGET
+path-constant TOP : . ;
+
+project 
+    : requirements
+    <library>/boost//headers
+    <library>/boost/program_options//boost_program_options
+    <toolset>gcc:<cxxflags>-fno-strict-aliasing
+    <toolset>gcc:<cxxflags>--std=c++0x
+    ;
 
 $TARGETS
-
-clean :
-    rm *.o *~ $TARGET
 """
 
 
@@ -34,7 +35,7 @@ class simplemakefile( code_template_helpers.APlugin ):
 
     def register_in_arg_parser( self , subparsers ):
         parser = code_template_helpers.create_subparser( self , subparsers )
-        parser.add_argument( "-f" , "--filename" ,  nargs = "+" , help = filename_help , default=["Makefile"] )
+        parser.add_argument( "-f" , "--filename" ,  nargs = "+" , help = filename_help , default=["Jamroot"] )
         parser.add_argument( "-t" , "--target" , nargs = "*" , help = target_help )
 
 
@@ -42,13 +43,15 @@ class simplemakefile( code_template_helpers.APlugin ):
     def do_work( self , args , replacements ):
         print "Creating " + self.name + " template(s) ..."
 
-        replacements[ "TARGET" ] = ""
         replacements[ "TARGETS" ] = ""
         if ( hasattr( args , "target" ) ) and ( args.target is not None ) and ( len( args.target ) != 0 ) :
             for target in args.target:
                 print "* Found target " + target
-                replacements[ "TARGET" ] += ( target + " " )
-                replacements[ "TARGETS" ] += ( target + " : " + target + ".o" + "\n" )
+                p = os.path.splitext( target )
+                if p[1] == ".cpp" :
+                    replacements[ "TARGETS" ] += "exe " + p[0] + " : " + p[0] + ".cpp" + "\n"
+                else:
+                    replacements[ "TARGETS" ] += "exe " + target + " : " + target + ".cpp" + "\n"
             
         if hasattr( args , "filename" ) :
             for filename in args.filename:
